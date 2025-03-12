4bede85c7fb1312ad2edc2097d8f7b8aa38db978
Tyler Truong <tylerttruong@cpp.edu>
2025-03-11 22:41:04 -0700

    Updated tests for multiplication

--- test_calculator.c
+++ test_calculator.c
@@ -66,6 +66,33 @@ void test_sub_underflow(void) {
     TEST_ASSERT_TRUE(result > 0);
 }
 
+void test_mult_positive_numbers(void) {
+    TEST_ASSERT_EQUAL(8, mult(2, 4));
+}
+
+void test_mult_positive_and_negative_numbers(void) {
+    TEST_ASSERT_EQUAL(-6, mult(3, -2));
+}
+
+void test_mult_negative_numbers(void) {
+    TEST_ASSERT_EQUAL(10, mult(-2, -5));
+}
+
+void test_mult_zero(void) {
+    TEST_ASSERT_EQUAL(0, mult(10, 0));
+    TEST_ASSERT_EQUAL(0, mult(0, 0));
+}
+
+void test_mult_overflow(void) {
+    int result = mult(INT_MAX, 2);
+    TEST_ASSERT_TRUE(result < 0);
+}
+
+void test_mult_underflow(void) {
+    int result = mult(INT_MIN, 2);
+    TEST_ASSERT_TRUE(result > 0);
+}
+
 int main(void) {
     UNITY_BEGIN();
     RUN_TEST(test_add_positive_numbers);    //Run our test function
@@ -80,5 +107,11 @@ int main(void) {
     RUN_TEST(test_sub_zero);
     RUN_TEST(test_sub_overflow);
     RUN_TEST(test_sub_underflow);
+    RUN_TEST(test_mult_positive_numbers);  
+    RUN_TEST(test_mult_positive_and_negative_numbers);;
+    RUN_TEST(test_mult_negative_numbers);
+    RUN_TEST(test_mult_zero);
+    RUN_TEST(test_mult_overflow);
+    RUN_TEST(test_mult_underflow);
     return UNITY_END();
 }
