2627c5c47be92ce3f3f68ee7b9656323b55ae57c
Tyler Truong <tylerttruong@cpp.edu>
2025-03-11 19:01:44 -0700

    Updated add test for overflow and underflow

--- test_calculator.c
+++ test_calculator.c
@@ -29,11 +29,23 @@ void test_add_zero(void) {
     TEST_ASSERT_EQUAL(0, add(0, 0));
 }
 
+void test_add_overflow(void) {
+    int result = add(INT_MAX, 1);   //Should wrap around
+    TEST_ASSERT_TRUE(result < 0);   //Checks if overflow
+}
+
+void test_add_underflow(void) {
+    int result = add(INT_MIN, -1);
+    TEST_ASSERT_TRUE(result > 0);
+}
+
 int main(void) {
     UNITY_BEGIN();
     RUN_TEST(test_add_positive_numbers);    //Run our test function
     RUN_TEST(test_add_positive_and_negative_numbers);;
     RUN_TEST(test_add_negative_numbers);
     RUN_TEST(test_add_zero);
+    RUN_TEST(test_add_overflow);
+    RUN_TEST(test_add_underflow);
     return UNITY_END();
 }
